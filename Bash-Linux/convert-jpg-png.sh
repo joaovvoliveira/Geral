2cbae5f5eb2c1c8d262376ff22f73289a294779b
Victor <jvictor_oliveira@outlook.com.br>
2025-02-03 20:28:12 +0000

    Script to collect status for memory

--- Bash-Linux/convert-jpg-png.sh
+++ Bash-Linux/convert-jpg-png.sh
@@ -4,7 +4,7 @@
 #
 #
 
-converter_imagem(){
+function converte_imagem(){
 
 cd ./imagens-livros/
 
@@ -16,11 +16,33 @@ fi
 for image in *
 
 do
-	imagem_sem_extencao=$(ls $image | awk -F. '{ print $1 }') #armazena o valor do texto da imagem antes do .
+	local imagem_sem_extencao=$(ls $image | awk -F. '{ print $1 }') #armazena o valor do texto da imagem antes do .
 	convert ./$imagem_sem_extencao.jpg ./png/$imagem_sem_extencao.png #adiciona a imagem_sem_extencao ao tipo png, e entao converte dentro da pasta criada anteriormente
+	echo "$imagem_sem_extencao.png convertida com sucesso"
 done
-        echo "Execucao realizada com sucesso!"
 }
 
+converte_imagem 2> erros.log
+if [ $? -eq 0 ] # $? pega o valor de saida da funcao e valida se for igual a zero significa que deu tudo certo
+	echo "Cadastro de boa"
+else
+	echo "Deu erro"
+fi
 
-converter_imagem
+
+
+#function verifica_diretorio(){
+#    local diretorio="$1"
+#    local arquivo
+#    for arquivo in "$diretorio"/*; 
+#    do
+#     	if [ -f "$arquivo" ] then
+#        	converte_imagem
+#      	elif [ -d "$arquivo" ]; then # -d verifica se o arquivo e uma diretorio, se sim, ja dentro do diretorio ele executa novamente a funcao no diretorio atual.
+#            	verifica_diretorio "$arquivo"
+#	fi
+#    done
+#}
+
+
+#verifica_diretorio
